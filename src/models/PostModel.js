d6d00748e765733668abec84361b006db6d1ca52
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-10-31 20:32:46 +1100

    feat: add crud and seed

--- src/models/PostModel.js
+++ src/models/PostModel.js
@@ -15,8 +15,10 @@ const PostSchema = new mongoose.Schema({
 		// default: new Date(Date.now()) // Mongoose will run Date.now once when the app loads this file 
 	},
 });
+
 // 2. Make a model based on the schema
 const PostModel = mongoose.model("Post", PostSchema);
+
 // 3. Export the model for the rest of our code to use 
 module.exports = {
 	PostModel
