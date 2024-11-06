d6ef75f418ba0a9193938ea7031dc13a14612e91
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-11-06 14:38:37 +1100

    feat: fix seeding for subdoc

--- src/models/PostModel.js
+++ src/models/PostModel.js
@@ -12,7 +12,7 @@ const PostSchema = new mongoose.Schema({
 		trim: true
 		// unique:true
 	},
-	// content: String, 
+	//content: String, 
 	content: [LocalizedContentSchema],
 	date: {
 		type: Date,
